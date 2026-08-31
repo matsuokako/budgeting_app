ad9eedbc79b1e9cf27d79b5d9979f6f29c62a25f
kouki <koukou1380@icloud.com>
2026-09-01 07:13:13 +0900

    add

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
   const [amount, setAmount] = useState('')
   const [category, setCategory] = useState('食費')
   const [date, setDate] = useState('')
+  const [selectedMonth, setSelectedMonth] = useState(
+    new Date().toISOString().slice(0, 7)
+  )
   const [expenses, setExpenses] = useState<Expense[]>([])
 
   // 追加機能
@@ -35,7 +38,13 @@ function App() {
     setExpenses(newExpenses)
   }
 
-  const total = expenses.reduce(
+  // 選択した月の支出を取り出す
+  const filteredExpenses = expenses.filter(
+    (expense) => expense.date.slice(0, 7) === selectedMonth
+  )
+
+  // 合計
+  const total = filteredExpenses.reduce(
     (sum, expense) => sum + expense.amount,
     0
   )
@@ -48,11 +57,23 @@ function App() {
       <br></br>
       </header>
 
+      <h2>表示する月</h2><br/>
+      <p>表示する月を選択してください。</p>
+
+      <input
+        type='month'
+        value={selectedMonth}
+        onChange={(e) => setSelectedMonth(e.target.value)}
+      /><br/>
+
       <main className='container'>
+      <br/><br/>
       <h2>今月の支出</h2>
       <p>¥{total}</p>
+      <br/><br/>
 
       <div>
+      <p>年月日と金額、ジャンルを選択して支出を追加してください。</p>
       <input
         type='date'
         value={date}
@@ -81,10 +102,11 @@ function App() {
       </button>
     </div>
 
+      <br/>
       <h2>支出一覧</h2>
 
       <ul>
-        {expenses.map((expense, index) => (
+        {filteredExpenses.map((expense, index) => (
           <li key={index}>
             {expense.date} {expense.category}:￥{expense.amount}
 
